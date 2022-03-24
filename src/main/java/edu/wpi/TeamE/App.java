54a677b2ac890cb817659a151f64f7b12bcfab15
Tim Connors <timconnors33@gmail.com>
2022-03-24 19:15:20 -0400

    Medical equipment button and back button functionality added

--- src/main/java/edu/wpi/TeamE/App.java
+++ src/main/java/edu/wpi/TeamE/App.java
@@ -2,14 +2,25 @@ package edu.wpi.TeamE;
 
 import java.io.IOException;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class App extends Application {
+
+  @FXML Button medicalEquipmentButton;
+  @FXML Button backButton;
+  @FXML Button submitRequestButton;
+
+  FXMLLoader loader = new FXMLLoader();
+  Parent root;
 
   @Override
   public void init() {
@@ -18,9 +29,8 @@ public class App extends Application {
 
   @Override
   public void start(Stage primaryStage) throws IOException {
-    FXMLLoader loader = new FXMLLoader();
     loader.setLocation(getClass().getResource("view/defaultPage.fxml"));
-    Parent root = loader.load();
+    root = loader.load();
 
     Scene scene = new Scene(root);
 
@@ -28,6 +38,28 @@ public class App extends Application {
     primaryStage.setScene(scene);
     primaryStage.setResizable(false);
     primaryStage.show();
+  }
+
+  @FXML
+  private void backButton(ActionEvent event) throws IOException {
+    Node node = (Node) event.getSource();
+    Stage thisStage = (Stage) node.getScene().getWindow();
+
+    loader.setLocation(getClass().getResource("view/defaultPage.fxml"));
+    root = loader.load();
+
+    thisStage.setScene(new Scene(root));
+  }
+
+  @FXML
+  private void medicalEquipmentButton(ActionEvent event) throws IOException {
+    Node node = (Node) event.getSource();
+    Stage thisStage = (Stage) node.getScene().getWindow();
+
+    loader.setLocation(getClass().getResource("view/medicalEquipmentPage.fxml"));
+    root = loader.load();
+
+    thisStage.setScene(new Scene(root));
   }
 
   @Override
