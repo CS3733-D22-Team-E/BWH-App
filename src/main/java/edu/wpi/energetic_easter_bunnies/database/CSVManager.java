9e2f4b199fd6fdedc8d91d38d17657357a437724
RoeyJozman <jojo.rozman@gmail.com>
2022-03-28 13:37:46 -0400

    Basic Implementation of loading the location csv

--- src/main/java/edu/wpi/energetic_easter_bunnies/database/CSVManager.java
+++ src/main/java/edu/wpi/energetic_easter_bunnies/database/CSVManager.java
@@ -1,15 +1,55 @@
 package edu.wpi.energetic_easter_bunnies.database;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.sql.SQLException;
+import java.io.*;
+import java.sql.*;
 
 public class CSVManager {
+
   /**
-   * The user is prompted for the name of the CSV file. The program first loads all of the contents
-   * of the SQL Location table into Java Location objects. Then the CSV file is created from the
+   * Loads the location database from the location csv
+   * @param fileName - The file name where the database will be loaded from
+   */
+  public static void loadLocationCSV(String fileName) throws SQLException, IOException {
+    Connection connection = DriverManager.getConnection("jdbc:derby://localhost:1527/BWDB;");
+
+    String query = "INSERT INTO TOWERLOCATIONS (NODEID, XCOORD, YCOORD, FLOOR, BUILDING, " +
+            "NODETYPE, LONGNAME, SHORTNAME) VALUES (?, ?, ?, ?, ?, ?, ?, ?);";
+    PreparedStatement statement = connection.prepareStatement(query);
+
+    BufferedReader in = new BufferedReader(new FileReader(fileName));
+    String line;
+    in.readLine();
+
+    while((line = in.readLine()) != null) {
+      String[] data = line.split(",");
+      String nodeID = data[0];
+      int xcoord = Integer.parseInt(data[1]);
+      int ycoord = Integer.parseInt(data[2]);
+      String floor = data[3];
+      String building = data[4];
+      String nodeType = data[5];
+      String longName = data[6];
+      String shortName = data[7];
+
+      statement.setString(1, nodeID);
+      statement.setInt(2, xcoord);
+      statement.setInt(3, ycoord);
+      statement.setString(4, floor);
+      statement.setString(5, building);
+      statement.setString(6, nodeType);
+      statement.setString(7, longName);
+      statement.setString(8, shortName);
+
+      statement.addBatch();
+    }
+    in.close();
+    statement.executeBatch();
+    connection.commit();
+    connection.close();
+  }
+  /**
+   * The program first loads all of the contents of the SQL Location table
+   * into Java Location objects. Then the CSV file is created from the
    * Java objects.
    *
    * @param fileName - The file name where the CSV will be saved
