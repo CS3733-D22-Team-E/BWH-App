059e7c577230b035b98be8828f97ed455dc20f81
jjbornstein <jjbornstein@wpi.edu>
2022-03-29 01:39:23 -0400

    CSVmanager changes, Blocked from merge until other parts are implemented

---
+++ src/main/java/edu/wpi/energetic_easter_bunnies/database/MedicalEquipmentServiceRequestDAOImpl.java
@@ -0,0 +1,22 @@
+package edu.wpi.energetic_easter_bunnies.database;
+
+import java.util.List;
+
+public class MedicalEquipmentServiceRequestDAOImpl implements MedicalEquipmentServiceRequestDAO{
+
+    public List<ServiceRequest> getAllMedicalEquipmentServiceRequests() {
+        return null;
+    }
+
+    public ServiceRequest getMedicalEquipmentServiceRequest(int numID) {
+        return null;
+    }
+
+    public void updateMedicalEquipmentServiceRequest(ServiceRequest serviceRequest) {
+
+    }
+
+    public void deleteMedicalEquipmentServiceRequest(ServiceRequest serviceRequest) {
+
+    }
+}
