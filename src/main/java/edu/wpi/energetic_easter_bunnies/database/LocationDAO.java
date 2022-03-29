e1a136cc723dc62c56ef92ae38ecf96c475f9ca0
FrancArsen <sfang3@wpi.edu>
2022-03-29 15:49:58 -0400

    LocationDAO and implementation

--- src/main/java/edu/wpi/energetic_easter_bunnies/database/LocationDAO.java
+++ src/main/java/edu/wpi/energetic_easter_bunnies/database/LocationDAO.java
@@ -1,13 +1,18 @@
 package edu.wpi.energetic_easter_bunnies.database;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface LocationDAO {
-  List<Location> getAllLocations();
+    List<Location> getAllLocations();
 
-  Location getLocation(int numID);
+    Location getLocation(int numID);
 
-  void updateLocation(Location location);
+    void printLocations();
 
-  void deleteLocation(Location location);
+    void addLocation(Location location) throws SQLException;
+
+    void updateLocation(Location location, String newFloor, String newNodeType) throws SQLException;
+
+    void deleteLocation(Location location) throws SQLException;
 }
--- src/main/java/edu/wpi/energetic_easter_bunnies/database/LocationDAOImpl.java
+++ src/main/java/edu/wpi/energetic_easter_bunnies/database/LocationDAOImpl.java
@@ -5,76 +5,141 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LocationDAOImpl implements LocationDAO {
-  List<Location> locations;
+    List<Location> locations;
 
-  /**
-   * No args constructor that connects to the database to populate the locations list
-   *
-   * @throws SQLException - Database Connection
-   */
-  public LocationDAOImpl() throws SQLException {
-    locations = new ArrayList<>();
-    String url = "jdbc:derby://localhost:1527/BWDB;";
-    Connection connection = DriverManager.getConnection(url);
-    Statement statement = connection.createStatement();
-    String query = "SELECT * FROM TOWERLOCATIONS ORDER BY FLOOR DESC";
-    ResultSet rs = statement.executeQuery(query);
-    int numID = 0;
-    while (rs.next()) {
-      String nodeID = rs.getString("nodeID");
-      int xcoord = rs.getInt("xcoord");
-      int ycoord = rs.getInt("ycoord");
-      String floor = rs.getString("floor");
-      String building = rs.getString("building");
-      String nodeType = rs.getString("nodeType");
-      String longName = rs.getString("longName");
-      String shortName = rs.getString("shortName");
+    /**
+     * No args constructor that connects to the database to populate the locations list
+     *
+     * @throws SQLException - Database Connection
+     */
+    public LocationDAOImpl() throws SQLException {
+        locations = new ArrayList<>();
+        String url = "jdbc:derby://localhost:1527/BWDB;";
+        Connection connection = DriverManager.getConnection(url);
+        Statement statement = connection.createStatement();
+        String query = "SELECT * FROM TOWERLOCATIONS ORDER BY FLOOR DESC";
+        ResultSet rs = statement.executeQuery(query);
+        int numID = 0;
+        while (rs.next()) {
+            String nodeID = rs.getString("nodeID");
+            int xcoord = rs.getInt("xcoord");
+            int ycoord = rs.getInt("ycoord");
+            String floor = rs.getString("floor");
+            String building = rs.getString("building");
+            String nodeType = rs.getString("nodeType");
+            String longName = rs.getString("longName");
+            String shortName = rs.getString("shortName");
 
-      Location location =
-          new Location(
-              nodeID, xcoord, ycoord, floor, building, nodeType, longName, shortName, numID);
+            Location location =
+                    new Location(
+                            nodeID, xcoord, ycoord, floor, building, nodeType, longName, shortName, numID);
 
-      locations.add(location);
-      numID++;
+            locations.add(location);
+            numID++;
+        }
+        rs.close();
     }
-    rs.close();
-  }
-  /**
-   * Creates an arraylist of all locations from the db
-   *
-   * @return Arraylist of locations
-   */
-  @Override
-  public List<Location> getAllLocations() {
-    return locations;
-  }
 
-  /**
-   * @param numID - The numerical id of the location node
-   * @return the location requested
-   */
-  @Override
-  public Location getLocation(int numID) {
-    return locations.get(numID);
-  }
+    /**
+     * Creates an arraylist of all locations from the db
+     *
+     * @return Arraylist of locations
+     */
+    @Override
+    public List<Location> getAllLocations() {
+        return locations;
+    }
 
-  /**
-   * Updates a given location's floor and node type
-   *
-   * @param location - The location to be added
-   */
-  @Override
-  public void updateLocation(Location location) {
-    locations.add(location);
-  }
+    /**
+     * Gets a location with given numID in the arrayList
+     * @param numID - The numerical id of the location node
+     * @return the location requested
+     */
+    @Override
+    public Location getLocation(int numID) {
+        return locations.get(numID);
+    }
 
-  /**
-   * Deletes a location entry
-   *
-   * @param location - The location to be deleted
-   */
-  @Override
-  public void deleteLocation(Location location) {
-    locations.remove(location);
-  }
+    /**
+     * Prints all the location information
+     *
+     */
+    @Override
+    public void printLocations() {
+        for (Location location : locations) {
+            System.out.println(location);
+        }
+    }
+
+    /**
+     * Creates a new location entry in the database
+     *
+     * @param location - new location to be added
+     * @throws SQLException - Accesses Database
+     */
+    @Override
+    public void addLocation(Location location) throws SQLException {
+
+        String url = "jdbc:derby://localhost:1527/BWDB";
+        Connection connection = DriverManager.getConnection(url);
+        Statement statement = connection.createStatement();
+        locations.add(location);
+
+        String query =
+                "INSERT INTO TOWERLOCATIONS (NODEID) VALUES ('"
+                        + location.getNodeID()
+                        + "')"; // Insert into database; does not check if the nodeID already exists
+        statement.executeUpdate(query);
+    }
+
+    /**
+     * Updates a given location's floor and node type
+     *
+     * @param location    - the location node to be updated
+     * @param newFloor    - the new floor value
+     * @param newNodeType - the new node type
+     * @throws SQLException - Accesses Database
+     */
+    @Override
+    public void updateLocation(Location location, String newFloor, String newNodeType) throws SQLException {
+
+        // Updating location floor and node type
+        location.setFloor(newFloor);
+        location.setNodeType(newNodeType);
+
+        // Update location floor and node type in the db
+        String url = "jdbc:derby://localhost:1527/BWDB;";
+        Connection connection = DriverManager.getConnection(url);
+        Statement statement = connection.createStatement();
+        String query =
+                "UPDATE TOWERLOCATIONS SET FLOOR = '"
+                        + newFloor
+                        + "', NODETYPE = '"
+                        + newNodeType
+                        + "' WHERE NODEID = '"
+                        + location.getNodeID()
+                        + "'";
+        statement.executeUpdate(query);
+    }
+
+    /**
+     * Deletes a location entry in the database
+     *
+     * @param location - location to be deleted
+     * @throws SQLException - Accesses Database
+     */
+
+    @Override
+    public void deleteLocation(Location location) throws SQLException {
+
+        // Deleting the location from the array list
+        locations.remove(location);
+
+        // Remove location in the db
+        String url = "jdbc:derby://localhost:1527/BWDB";
+        Connection connection = DriverManager.getConnection(url);
+        Statement statement = connection.createStatement();
+        String query = "DELETE FROM TOWERLOCATIONS WHERE nodeID = ('" + location.getNodeID() + "')";
+        statement.executeUpdate(query);
+    }
 }
