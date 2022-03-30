83075471e4755785580a9d9634127b98355dcc18
pbrush <pdbrush@WPI.edu>
2022-03-30 15:24:58 -0400

    Submit request button is working now, service page should be done.

--- src/main/java/edu/wpi/energetic_easter_bunnies/controllers/MealDeliveryController.java
+++ src/main/java/edu/wpi/energetic_easter_bunnies/controllers/MealDeliveryController.java
@@ -7,9 +7,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
 public class MealDeliveryController extends ServiceRequestPageController {
-  @FXML MenuButton entreeDropDown;
-  @FXML MenuButton beverageDropDown;
-  @FXML MenuButton dessertDropDown;
+  @FXML ComboBox<String> entreeDropDown;
+  @FXML ComboBox<String> beverageDropDown;
+  @FXML ComboBox<String> dessertDropDown;
   @FXML TextField roomNumberTxt;
   @FXML DatePicker dateTime;
   @FXML TextField timeTxt;
@@ -23,17 +23,39 @@ public class MealDeliveryController extends ServiceRequestPageController {
   @FXML
   public void submitButton(ActionEvent event) {
     try {
-      mealDeliveryRequest.setEntreeType(entreeDropDown.getText()); // need to figure this out
-      mealDeliveryRequest.setBeverageType(beverageDropDown.getText());
-      mealDeliveryRequest.setDessertType(dessertDropDown.getText());
-      mealDeliveryRequest.setRoomNumber(Integer.parseInt(roomNumberTxt.getText()));
+      mealDeliveryRequest.setEntreeType(entreeDropDown.getValue());
+      mealDeliveryRequest.setBeverageType(beverageDropDown.getValue());
+      mealDeliveryRequest.setDessertType(dessertDropDown.getValue());
+      mealDeliveryRequest.setRoomNumber(Integer.parseInt("0" + roomNumberTxt.getText()));
       mealDeliveryRequest.setDeliveryDate(dateTime.getValue());
-      mealDeliveryRequest.setDeliveryTime(Integer.parseInt(timeTxt.getText()));
+      mealDeliveryRequest.setDeliveryTime(Integer.parseInt("0" + timeTxt.getText()));
       mealDeliveryRequest.setASAP(asapCheck.isSelected());
       mealDeliveryRequest.setOtherNotes(otherNotesTxt.getText());
+      if (entreeDropDown.getValue().isEmpty()
+          || entreeDropDown.getValue().isBlank()
+          || beverageDropDown.getValue().isEmpty()
+          || beverageDropDown.getValue().isBlank()
+          || dessertDropDown.getValue().isEmpty()
+          || dessertDropDown.getValue().isBlank()
+          || dateTime.getValue().equals(0)
+          || dateTime.getValue().equals(0)) {
+        throw new NullPointerException();
+      }
     } catch (NullPointerException error) {
       System.out.println("Error : Some Value is NULL");
       PopUpWarning.createWarning("Warning : A required value was not filled");
     }
   }
+
+  //  @FXML
+  //  private void resetButton(ActionEvent event) {
+  //    entreeDropDown.clearSelection();
+  //    room.getSelectionModel().clearSelection();
+  //    equipmentType.getSelectionModel().clearSelection();
+  //    equipmentQuantity.getSelectionModel().clearSelection();
+  //    deliveryDate.clear();
+  //    deliveryTime.clear();
+  //    isUrgent.setSelected(false);
+  //    notes.clear();
+  //  }
 }
