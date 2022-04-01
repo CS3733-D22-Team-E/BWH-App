76ec2ad197cbc83dd63e0b1a0859fc823b144083
Colinm1215 <colinm1215@gmail.com>
2022-04-01 17:41:27 -0400

    update DerbyDriver

--- src/main/java/edu/wpi/energetic_easter_bunnies/Main.java
+++ src/main/java/edu/wpi/energetic_easter_bunnies/Main.java
@@ -3,6 +3,15 @@ package edu.wpi.energetic_easter_bunnies;
 public class Main {
 
   public static void main(String[] args) {
+    try {
+      Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+    } catch (ClassNotFoundException e) {
+      System.out.println("Apache Derby Driver not found.");
+      e.printStackTrace();
+      return;
+    }
+
+    System.out.println("Apache Derby driver registered!");
     App.launch(App.class, args);
   }
 }
