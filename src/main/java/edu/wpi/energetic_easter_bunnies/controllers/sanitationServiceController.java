aa768d70a2779e7d347f0d02b8871ba55c1cf596
Colinm1215 <colinm1215@gmail.com>
2022-04-03 14:25:49 -0400

    sideMenu

--- src/main/java/edu/wpi/energetic_easter_bunnies/controllers/sanitationServiceController.java
+++ src/main/java/edu/wpi/energetic_easter_bunnies/controllers/sanitationServiceController.java
@@ -1,17 +1,60 @@
 package edu.wpi.energetic_easter_bunnies.controllers;
 
+import com.jfoenix.controls.JFXDrawer;
+import com.jfoenix.controls.JFXHamburger;
+import com.jfoenix.transitions.hamburger.HamburgerSlideCloseTransition;
+import edu.wpi.energetic_easter_bunnies.Main;
 import edu.wpi.energetic_easter_bunnies.PopUpWarning;
 import edu.wpi.energetic_easter_bunnies.entity.sanitationRequest;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 
-public class sanitationServiceController extends serviceRequestPageController {
+public class sanitationServiceController extends serviceRequestPageController
+    implements Initializable {
 
+  @FXML JFXHamburger burger;
+  @FXML JFXDrawer drawer;
   @FXML TextField locationField;
   @FXML ToggleGroup biohazardGroup;
   @FXML ToggleGroup urgencyGroup;
   @FXML ToggleGroup sizeGroup;
+  VBox box;
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    HamburgerSlideCloseTransition transition = new HamburgerSlideCloseTransition(burger);
+    FXMLLoader l = new FXMLLoader();
+    l.setLocation(Main.class.getResource("view/sidePanel.fxml"));
+    try {
+      box = l.load();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    drawer.setSidePane(box);
+
+    transition.setRate(-1);
+    burger.addEventHandler(
+        MouseEvent.MOUSE_PRESSED,
+        (e) -> {
+          transition.setRate(transition.getRate() * -1);
+          transition.play();
+          if (drawer.isOpened()) {
+            drawer.close();
+            box.setDisable(true);
+          } else {
+            drawer.open();
+            box.setDisable(false);
+          }
+        });
+  }
 
   public sanitationServiceController() {}
 
