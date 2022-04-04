723de38c94449879c0c16780d6be593d0ed8dda7
pbrush <pdbrush@WPI.edu>
2022-04-04 19:07:08 -0400

    resolving git problems.

--- src/main/java/edu/wpi/energetic_easter_bunnies/controllers/mealDeliveryController.java
+++ src/main/java/edu/wpi/energetic_easter_bunnies/controllers/mealDeliveryController.java
@@ -107,6 +107,8 @@ public class mealDeliveryController extends serviceRequestPageController impleme
           || dateTime.getValue().equals(0)
           || dateTime.getValue().equals(0)
           || staffAssignee.getText().isEmpty()
+          || staffAssignee.getText().isBlank()
+          || requestStatus.getText().isBlank()
           || requestStatus.getText().isEmpty()) {
         throw new NullPointerException();
       }
