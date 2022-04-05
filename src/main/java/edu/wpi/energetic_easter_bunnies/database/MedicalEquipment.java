d8200e131c1f37f9757a2ea908ecdcd32d49465b
Eric Zhou <zdog74721@gmail.com>
2022-04-05 00:10:01 -0400

    fixed getXCoord() and getYCoord(), as well as implementing getFloor() in MedicalEquipment.java

--- src/main/java/edu/wpi/energetic_easter_bunnies/database/MedicalEquipment.java
+++ src/main/java/edu/wpi/energetic_easter_bunnies/database/MedicalEquipment.java
@@ -8,7 +8,16 @@ import java.sql.Statement;
 public class MedicalEquipment extends Equipment {
   private String med_equipmentID;
   private String equipmentType;
-
+/*
+  public static void main(String[] args) throws SQLException {
+    MedicalEquipment equip =
+        new MedicalEquipment(
+            "BED01", "Balls", false, true, "OR BED Park", "OR BED Park", "ABBPRK03", "BED", 5);
+    System.out.println(equip.getXCoord());
+    System.out.println(equip.getYCoord());
+    System.out.println(equip.getFloor());
+  }
+*/
   public MedicalEquipment(
       String equipmentID,
       String med_equipmendID,
@@ -66,7 +75,9 @@ public class MedicalEquipment extends Equipment {
     String query = "SELECT XCOORD FROM TOWERLOCATIONS WHERE NODEID = '" + locationID + "'";
     ResultSet rs = statement.executeQuery(query);
 
-    result = rs.getInt("XCOORD");
+    if (rs.next()) {
+      result = rs.getInt("XCOORD");
+    }
     return result;
   }
 
@@ -79,7 +90,24 @@ public class MedicalEquipment extends Equipment {
     String query = "SELECT YCOORD FROM TOWERLOCATIONS WHERE NODEID = '" + locationID + "'";
     ResultSet rs = statement.executeQuery(query);
 
-    result = rs.getInt("YCOORD");
+    if (rs.next()) {
+      result = rs.getInt("YCOORD");
+    }
+    return result;
+  }
+
+  public String getFloor() throws SQLException {
+    Connection connection = DBConnection.getConnection();
+    String locationID = getCurrentLocation();
+    String result = "";
+
+    Statement statement = connection.createStatement();
+    String query = "SELECT FLOOR FROM TOWERLOCATIONS WHERE NODEID = '" + locationID + "'";
+    ResultSet rs = statement.executeQuery(query);
+
+    if (rs.next()) {
+      result = rs.getString("FLOOR");
+    }
     return result;
   }
 }
