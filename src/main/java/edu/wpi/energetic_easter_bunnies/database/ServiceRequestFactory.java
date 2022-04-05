99b377c1c63e56962c3d4e914af7905c730f613b
RoeyJozman <jojo.rozman@gmail.com>
2022-04-05 11:34:53 -0400

    ServiceRequestFactory implementation

---
+++ src/main/java/edu/wpi/energetic_easter_bunnies/database/ServiceRequestFactory.java
@@ -0,0 +1,15 @@
+package edu.wpi.energetic_easter_bunnies.database;
+
+import edu.wpi.energetic_easter_bunnies.entity.*;
+
+public class ServiceRequestFactory {
+    public serviceRequest getServiceRequest(String serviceRequestType) {
+        if(serviceRequestType == null) return null;
+        if(serviceRequestType.equalsIgnoreCase("MED_EQUIP_REQ")) return new medicalEquipmentRequest();
+        else if(serviceRequestType.equalsIgnoreCase("LAB_REQUEST")) return new labRequest();
+        else if(serviceRequestType.equalsIgnoreCase("LANG_INTERP_REQ")) return new languageInterpreterRequest();
+        else if(serviceRequestType.equalsIgnoreCase("MEAL_DELIV_REQ")) return new mealDeliveryRequest();
+        else if(serviceRequestType.equalsIgnoreCase("SANITATION_REQ")) return new sanitationRequest();
+        return null;
+    }
+}
