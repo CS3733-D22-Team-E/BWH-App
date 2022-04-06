f5b2af922a392cbd8e37d76fc311da79a4bbea87
Colinm1215 <colinm1215@gmail.com>
2022-04-05 20:03:57 -0400

    fixed (kinda)

--- src/main/java/edu/wpi/energetic_easter_bunnies/controllers/languageInterpreterRequestController.java
+++ src/main/java/edu/wpi/energetic_easter_bunnies/controllers/languageInterpreterRequestController.java
@@ -2,6 +2,8 @@ package edu.wpi.energetic_easter_bunnies.controllers;
 
 import edu.wpi.energetic_easter_bunnies.PopUpWarning;
 import edu.wpi.energetic_easter_bunnies.entity.languageInterpreterRequest;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -10,16 +12,16 @@ import javafx.scene.control.*;
 public class languageInterpreterRequestController extends serviceRequestPageController
     implements Initializable {
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-      super.initialize(location,resources);
-       /*floorSelection.setValue("Select Floor");
-      roomSelection.setValue("Select Room");
-      languageSelection.setValue("Select Language");
-      floorSelection.getItems().addAll(floorList);
-      roomSelection.getItems().addAll(roomList);
-      languageSelection.getItems().addAll(langList);*/
-    }
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    super.initialize(location, resources);
+    floorSelection.setValue("Select Floor");
+    roomSelection.setValue("Select Room");
+    languageSelection.setValue("Select Language");
+    floorSelection.getItems().addAll(floorList);
+    roomSelection.getItems().addAll(roomList);
+    languageSelection.getItems().addAll(langList);
+  }
 
   @FXML private ChoiceBox<String> floorSelection;
 
@@ -41,7 +43,7 @@ public class languageInterpreterRequestController extends serviceRequestPageCont
   @FXML
   public void submitButton(ActionEvent event) {
     try {
-      /* languageInterpreterRequest.setFloorSelected(floorSelection.getValue());
+      /*      languageInterpreterRequest.setFloorSelected(floorSelection.getValue());
       languageInterpreterRequest.setLanguageSelected(languageSelection.getValue());
       languageInterpreterRequest.setRoomSelected(roomSelection.getValue());
       languageInterpreterRequest.setEndDate(endSelection.getValue());
