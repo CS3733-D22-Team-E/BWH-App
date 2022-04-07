c4b4d04d0a8ce4e0c77848a0d698eae3cb86330a
Colinm1215 <colinm1215@gmail.com>
2022-04-07 18:24:41 -0400

    Fixed RSA
    Keys are now saved, they are then loaded
    This ensures keys are maintained between instances of the program - allowing for keys to be saved in DB and still verified

--- src/main/java/edu/wpi/energetic_easter_bunnies/RSAEncryption.java
+++ src/main/java/edu/wpi/energetic_easter_bunnies/RSAEncryption.java
@@ -1,10 +1,8 @@
 package edu.wpi.energetic_easter_bunnies;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.security.*;
-import java.security.spec.EncodedKeySpec;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
@@ -15,8 +13,7 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 
 public class RSAEncryption {
-  private static KeyPairGenerator generator;
-  private static KeyPair pair;
+  private static KeyFactory kf;
   private static PrivateKey privateKey;
   private static PublicKey publicKey;
   private static Cipher encryptCipher;
@@ -24,34 +21,32 @@ public class RSAEncryption {
 
   static {
     try {
-      generator = KeyPairGenerator.getInstance("RSA");
-      generator.initialize(2048);
-      pair = generator.generateKeyPair();
-      privateKey = pair.getPrivate();
-      publicKey = pair.getPublic();
+      kf = KeyFactory.getInstance("RSA");
 
-      InputStream stream = Main.class.getResourceAsStream("rsa/public.key");
-      assert stream != null;
-      byte[] publicKeyBytes = stream.readAllBytes();
-      KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-      EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(publicKeyBytes);
-      keyFactory.generatePublic(publicKeySpec);
+      FileInputStream inputStream = new FileInputStream("rsa/public.key");
 
-      stream = Main.class.getResourceAsStream("rsa/private.key");
-      assert stream != null;
-      byte[] privateKeyBytes = stream.readAllBytes();
-      EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
-      keyFactory.generatePrivate(privateKeySpec);
+      byte[] publicKeyBytes = inputStream.readAllBytes();
+      X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(publicKeyBytes);
+
+      publicKey = kf.generatePublic(publicKeySpec);
+
+      inputStream = new FileInputStream("rsa/private.key");
+
+      byte[] privateKeyBytes = inputStream.readAllBytes();
+      PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
+
+      privateKey = kf.generatePrivate(privateKeySpec);
 
       encryptCipher = Cipher.getInstance("RSA");
       encryptCipher.init(Cipher.ENCRYPT_MODE, publicKey);
 
       decryptCipher = Cipher.getInstance("RSA");
       decryptCipher.init(Cipher.DECRYPT_MODE, privateKey);
-    } catch (NoSuchPaddingException
-        | NoSuchAlgorithmException
+
+    } catch (NoSuchAlgorithmException
         | IOException
         | InvalidKeySpecException
+        | NoSuchPaddingException
         | InvalidKeyException e) {
       e.printStackTrace();
     }
