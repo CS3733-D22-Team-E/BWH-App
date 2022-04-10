e966bb16b16249a10a4de056eb76c4146e20b481
RoeyJozman <jojo.rozman@gmail.com>
2022-04-10 14:57:21 -0400

    Beginning of Facade Impl

---
+++ src/main/java/edu/wpi/energetic_easter_bunnies/database/daos/DAOSystem.java
@@ -0,0 +1,24 @@
+package edu.wpi.energetic_easter_bunnies.database.daos;
+
+import java.sql.SQLException;
+
+public class DAOSystem {
+    private EmployeeDAOImpl employeeDAO;
+    private LabRequestDAOImpl labRequestDAO;
+    private LocationDAOImpl locationDAO;
+    private MedicalEquipmentDAOImpl medicalEquipmentDAO;
+    private MedicalEquipmentServiceRequestDAOImpl medicalEquipmentServiceRequestDAO;
+    private ServiceRequestDAOImpl serviceRequestDAO;
+
+    public DAOSystem() throws SQLException {
+        employeeDAO = new EmployeeDAOImpl();
+        labRequestDAO = new LabRequestDAOImpl();
+        locationDAO = new LocationDAOImpl();
+        medicalEquipmentDAO = new MedicalEquipmentDAOImpl();
+        medicalEquipmentServiceRequestDAO = new MedicalEquipmentServiceRequestDAOImpl();
+        serviceRequestDAO = new ServiceRequestDAOImpl();
+    }
+
+    //TODO Implement multiple constructors for any of subsystems that have multiple constructors
+    //TODO Implement all methods that are not overrides of DAO<T>
+}
