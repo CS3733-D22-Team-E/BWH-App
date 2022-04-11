fd460cfe57e5d879c29990deda3f4a17009b0bfc
Colinm1215 <colinm1215@gmail.com>
2022-04-11 12:19:18 -0400

    side menu fixes

--- src/main/java/edu/wpi/energetic_easter_bunnies/controllers/containsSideMenu.java
+++ src/main/java/edu/wpi/energetic_easter_bunnies/controllers/containsSideMenu.java
@@ -2,23 +2,25 @@ package edu.wpi.energetic_easter_bunnies.controllers;
 
 import com.jfoenix.controls.JFXDrawer;
 import com.jfoenix.controls.JFXHamburger;
+import com.jfoenix.controls.events.JFXDrawerEvent;
 import com.jfoenix.transitions.hamburger.HamburgerSlideCloseTransition;
 import edu.wpi.energetic_easter_bunnies.Main;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 
 public class containsSideMenu implements Initializable {
   @FXML public JFXHamburger burger;
   @FXML JFXDrawer drawer;
+  // Node box;
   StackPane box;
-  ScrollPane scrollPane;
+  // ScrollPane box;
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
@@ -29,12 +31,24 @@ public class containsSideMenu implements Initializable {
     HamburgerSlideCloseTransition transition = new HamburgerSlideCloseTransition(burger);
     FXMLLoader l = new FXMLLoader();
     l.setLocation(Main.class.getResource("view/sidePanel.fxml"));
+    box = null;
     try {
       box = l.load();
     } catch (IOException e) {
       e.printStackTrace();
     }
+    assert (box != null);
     drawer.setSidePane(box);
+    drawer.setOnDrawerClosed(
+        new EventHandler<JFXDrawerEvent>() {
+          @Override
+          public void handle(JFXDrawerEvent event) {
+            box.setDisable(true);
+            drawer.setDisable(true);
+            transition.setRate(-1);
+            transition.play();
+          }
+        });
     box.setDisable(true);
     drawer.setDisable(true);
 
