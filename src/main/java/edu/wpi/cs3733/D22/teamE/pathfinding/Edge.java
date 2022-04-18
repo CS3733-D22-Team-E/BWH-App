e844db3c0c774af6523324858563b1b65b8adfdd
RoeyJozman <jojo.rozman@gmail.com>
2022-04-18 19:43:00 -0400

    Got Correct Edge Class

--- src/main/java/edu/wpi/cs3733/D22/teamE/pathfinding/Edge.java
+++ src/main/java/edu/wpi/cs3733/D22/teamE/pathfinding/Edge.java
@@ -4,34 +4,34 @@ import java.util.ArrayList;
 
 public class Edge {
 
-  private double length;
-  private ArrayList<Node> neighboringNodes;
+    private double length;
+    private ArrayList<Node> neighboringNodes;
 
-  public Edge() {}
+    public Edge() {}
 
-  public double getLength() {
-    return this.length;
-  }
-
-  public void setLength(double l) {
-    this.length = l;
-  }
-
-  public ArrayList<Node> getNeighbors() {
-    return this.neighboringNodes;
-  }
-
-  // What should this take in?
-  public void setNeighboringNode(Node n) {
-    this.neighboringNodes.add(n);
-  }
-
-  public Node getOppositeNode(Node n) {
-    for (Node m : this.getNeighbors()) {
-      if (m != n) {
-        return m;
-      }
+    public double getLength() {
+        return this.length;
     }
-    return null;
-  }
+
+    public void setLength(double l) {
+        this.length = l;
+    }
+
+    public ArrayList<Node> getNeighbors() {
+        return this.neighboringNodes;
+    }
+
+    // What should this take in?
+    public void setNeighboringNode(Node n) {
+        this.neighboringNodes.add(n);
+    }
+
+    public Node getOppositeNode(Node n) {
+        for (Node m : this.getNeighbors()) {
+            if (m != n) {
+                return m;
+            }
+        }
+        return null;
+    }
 }
