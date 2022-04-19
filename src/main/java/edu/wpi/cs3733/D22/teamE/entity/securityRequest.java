6b03f02d212a349b6536b81765f9ae02623958ab
FrancArsen <sfang3@wpi.edu>
2022-04-19 16:09:00 -0400

    Security tests

---
+++ src/main/java/edu/wpi/cs3733/D22/teamE/entity/securityRequest.java
@@ -0,0 +1,70 @@
+package edu.wpi.cs3733.D22.teamE.entity;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class securityRequest extends serviceRequest {
+  private String securityRequestType;
+  private String timeFrame;
+
+  private List<String> timeVals =
+      new ArrayList<>() {
+        {
+          add("ASAP");
+          add("<1 day");
+          add("<1 week");
+        }
+      };
+
+  public securityRequest(
+      String securityRequestID,
+      String securityRequestType,
+      String timeFrame,
+      String floorID,
+      String roomID,
+      boolean isUrgent,
+      String staffAssignee,
+      String requestStatus,
+      String otherNotes,
+      LocalDate requestDate,
+      LocalDate deliveryDate) {
+    super(
+        securityRequestID,
+        String.valueOf(Type.LAB_REQUEST),
+        otherNotes,
+        floorID,
+        roomID,
+        isUrgent,
+        requestStatus,
+        staffAssignee,
+        requestDate,
+        deliveryDate);
+    setLabRequestType(securityRequestType);
+    setTimeFrame(timeFrame);
+  }
+
+  public securityRequest() {
+    super(String.valueOf(Type.LAB_REQUEST));
+    this.securityRequestType = "";
+    this.timeFrame = "";
+  }
+
+  public String getSecurityRequestType() {
+    return securityRequestType;
+  }
+
+  public void setLabRequestType(String securityRequestType) {
+    this.securityRequestType = securityRequestType;
+  }
+
+  public void setTimeFrame(String timeFrame) {
+    if (!timeVals.contains(timeFrame))
+      throw new RuntimeException("TimeFrame must be one of " + timeVals);
+    else this.timeFrame = timeFrame;
+  }
+
+  public String getTimeFrame() {
+    return timeFrame;
+  }
+}
