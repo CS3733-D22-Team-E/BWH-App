cd6c1711afd10fdd504210cb0a94d0f3e51ed432
FrancArsen <sfang3@wpi.edu>
2022-04-19 21:20:41 -0400

    Update securityRequestController.java

--- src/main/java/edu/wpi/cs3733/D22/teamE/controllers/securityRequestController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamE/controllers/securityRequestController.java
@@ -97,7 +97,7 @@ public class securityRequestController extends serviceRequestPageController {
 
   @Override
   public void submitButton(ActionEvent event) throws SQLException {
-    securityReq.setServiceRequestID("2");
+    securityReq.setServiceRequestID("SecReq2");
     securityReq.setSecurityRequestType(securityRequestType.getValue());
     securityReq.setTimeFrame(timeFrameComboBox.getValue());
     securityReq.setRoomID(roomNameToRoomID.get(room.getValue()));
