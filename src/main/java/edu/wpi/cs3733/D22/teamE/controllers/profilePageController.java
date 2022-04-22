b70fdfb3a340dc3452ba735a127189004c3ccd4d
Colinm1215 <colinm1215@gmail.com>
2022-04-22 15:44:16 -0400

    password change fix

--- src/main/java/edu/wpi/cs3733/D22/teamE/controllers/profilePageController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamE/controllers/profilePageController.java
@@ -22,13 +22,11 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
-import javafx.stage.Stage;
 
 public class profilePageController extends containsSideMenu implements Initializable {
 
@@ -83,8 +81,6 @@ public class profilePageController extends containsSideMenu implements Initializ
 
   @FXML
   public void resetPassword(ActionEvent event) throws IOException {
-    Stage thisStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-    pageControlFacade.loadPage("passwordSettingPage.fxml", thisStage);
-    // pageControlFacade.loadPage("helpPage.fxml", thisStage);
+    pageControlFacade.loadPage("passwordSettingPage.fxml");
   }
 }
