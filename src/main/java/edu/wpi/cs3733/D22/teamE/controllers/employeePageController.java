a3c1b1c1ada62c8382be93aba9296e4d941df20e
hrzink <75690370+hrzink@users.noreply.github.com>
2022-04-26 21:28:48 -0400

    Changes to Employee Controller

--- src/main/java/edu/wpi/cs3733/D22/teamE/controllers/employeePageController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamE/controllers/employeePageController.java
@@ -1,3 +1,43 @@
 package edu.wpi.cs3733.D22.teamE.controllers;
 
-public class employeePageController {}
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import edu.wpi.cs3733.D22.teamE.database.daos.DAOSystem;
+import edu.wpi.cs3733.D22.teamE.database.daos.DAOSystemSingleton;
+import edu.wpi.cs3733.D22.teamE.entity.Employee;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
+public class employeePageController implements Initializable {
+
+  @FXML TextField firstName;
+  @FXML TextField lastName;
+  @FXML TextField position;
+  @FXML TextField employeeStatus;
+  @FXML TextField salary;
+  @FXML TextField Username;
+  @FXML TextField Password;
+  @FXML TextField submitButton;
+  @FXML TextField resetButton;
+
+
+  @FXML TableColumn tableEmployeeName;
+  @FXML TableColumn tablePosition;
+  @FXML TableColumn tableSalary;
+  @FXML TableColumn tableUsername;
+
+  DAOSystem system;
+
+  @Override
+  public void initialize(URL url, ResourceBundle resourceBundle) {
+    system = DAOSystemSingleton.INSTANCE.getSystem();
+
+    system.getAllEmployee();
+    
+    Employee employee = new Employee()
+
+  }
+}
